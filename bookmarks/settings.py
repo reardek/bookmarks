1a3ce5888dd30518c74097a7e86f31a9e32865b5
reardek <rebciopl@gmail.com>
2019-10-21 15:47:51 +0200

    fully functional login system

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+# Login attributes
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 # Application definition
 
 INSTALLED_APPS = [
